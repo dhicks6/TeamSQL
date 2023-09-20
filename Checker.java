b79570abd7bee4387034c0a691ab1b377c71f61e
dhicks6 <dhicks23@msudenver.edu>
2023-09-20 17:12:59 -0600

    First commit

---
+++ Checker.java
@@ -0,0 +1,35 @@
+public class Checker {
+    /*
+     1. Check to make sure all decision structures (if, if-else, if-else-if, switch) use curly braces
+     appropriately. If not, fix it. Assume that single statement block is also required to include
+     curly braces.
+     */
+
+    // Should first check for how many open and closed curly braces there are
+    // We can use a stack and push them on and pop them off as we go through the file
+    // If there are any left on the tack at the end, then we know there is an error
+    // Or if there are more pops than there are pushes we know there is an error
+
+
+    /*
+    2. Check to make sure all loop structures (while, do-while, for) use curly braces
+    appropriately. If not, fix it. Assume that single statement block is also required to include
+    curly braces
+     */
+
+    /*
+    3. Check to make sure all the method structure is syntactically correct. If not, fix it.
+     */
+
+
+    /*
+    4. Count how many time the keyword “public” is used as keywords in the input program.
+     */
+
+
+    /*
+    5. Then your program will print to a text file the original input program, the updated input
+    program, and the number of time keyword “print” is used.
+     */
+
+}
